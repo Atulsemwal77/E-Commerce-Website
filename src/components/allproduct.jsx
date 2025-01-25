9f5770ada7fb20b832ae67017a3e226d8298cec4
Atul semwal <atulsemwal332@gmail.com>
2025-01-25 13:10:51 +0530

    change image

--- src/components/allproduct.jsx
+++ src/components/allproduct.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useCart } from './hm/CartContext'
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import one from "../assets/products/image1.png";
+import one from "../../New folder/image 46.png";
 import car from "../assets/products/car.png"
 
 const products = [
@@ -10,7 +10,8 @@ const products = [
     id: 1,
     name: "Breed Dry Dog Food",
     price: 100,
-    image: one,
+    // image: one,
+    image : one
     
   },
   {
@@ -18,8 +19,8 @@ const products = [
     name: "CANON EOS DSLR Camera",
     price: 100,
     // imageClass: "img10",
-    // image: "New folder/eos-250d-03-500x500 1.png",
-    image: one,
+    // image: "../assets/products/eos-250d-03-500x500 1.png",
+    // image: one,
     
   },
   {
@@ -27,8 +28,8 @@ const products = [
     name: "ASUS FHD Gaming Laptop",
     price: 700,
     // imageClass: "img9",
-    // image: "/New folder/ideapad-gaming-3i-01-500x500 1.png",
-    image: one
+    // image: "/../assets/products/ideapad-gaming-3i-01-500x500 1.png",
+    // image: one
     
   },
   {
@@ -36,7 +37,7 @@ const products = [
     name: "Curology Product Set",
     price: 500,
     // imageClass: "img9",
-    image: "New folder/Cart.png",
+    image: "../assets/products/Cart.png",
     
   },
   {
@@ -69,100 +70,100 @@ const products = [
     price: 660,
     // imageClass: "img9",
     image:
-      "New folder/698717_Z8A1X_3475_001_100_0000_Light-Reversible-quilted-satin-jacket 1.png",
+      "../assets/products/698717_Z8A1X_3475_001_100_0000_Light-Reversible-quilted-satin-jacket 1.png",
   },
   {
     id: 9,
     name: "HAVIT HV-G92 Gamepad",
     price: 120,
-    image : "New folder/g92-2-500x500 1.png"
+    image : "../assets/products/g92-2-500x500 1.png"
   },
   {
     id: 10,
     name: "IPS LCD Gaming Monitor",
     price: 370,
-    image: "New folder/g27cq4-500x500\ 1.png"
+    image: "../assets/products/g27cq4-500x500\ 1.png"
   },
   {
     id: 11,
     name: "Ak-900 Wired Keyboard",
     price: 960,
-    image: "New folder/ak-900-01-500x500\ 1.png"
+    image: "../assets/products/ak-900-01-500x500\ 1.png"
     
   },
   {
     id: 12,
     name: "S-Series Comfort Chair",
     price: 375,
-    image:"New folder/sam-moghadam-khamseh-kvmdsTrGOBM-unsplash\ 1.png"
+    image:"../assets/products/sam-moghadam-khamseh-kvmdsTrGOBM-unsplash\ 1.png"
 },
 {
   id: 13,
   name: 'Gucci Duffle Bag',
   price: 960,
-  image:"New folder/547953_9C2ST_8746_001_082_0000_Light-Gucci-Savoy-medium-duffle-bag 1.png",
+  image:"../assets/products/547953_9C2ST_8746_001_082_0000_Light-Gucci-Savoy-medium-duffle-bag 1.png",
   
 },
 {
   id: 14,
   name: 'The North Coat',
   price: 260,
-  image:"New folder/672462_ZAH9D_5626_002_100_0000_Light-The-North-Face-x-Gucci-coat 1.png",
+  image:"../assets/products/672462_ZAH9D_5626_002_100_0000_Light-The-North-Face-x-Gucci-coat 1.png",
   
 },
 {
   id: 15,
   name: 'Small BookSelf',
   price: 360,
-  image:"New folder/sam-moghadam-khamseh-L_7MQsHl_aU-unsplash 1.png",
+  image:"../assets/products/sam-moghadam-khamseh-L_7MQsHl_aU-unsplash 1.png",
   },
 {
   id: 16,
   name: 'RGB liquid CPU Cooler',
   price: 260,
-  image:"New folder/gammaxx-l240-argb-1-500x500 1.png",
+  image:"../assets/products/gammaxx-l240-argb-1-500x500 1.png",
   },
   {
     id: 17,
     name: 'Sony WH-1000XM4 Headphones',
     price: 350,
-    image: "New folder/download.jpeg",
+    image: "../assets/products/download.jpeg",
   },
   {
     id: 18,
     name: 'Apple MacBook Pro',
     price: 1999,
-    image: "New folder/download (1).jpeg",
+    image: "../assets/products/download (1).jpeg",
   },
   {
     id: 19,
     name: 'Samsung Galaxy S23',
     price: 1199,
-    image: "New folder/download (2).jpeg",
+    image: "../assets/products/download (2).jpeg",
   },
   {
     id: 20,
     name: 'Speaker',
     price: 150,
-    image: "New folder/download (4).jpeg",
+    image: "../assets/products/download (4).jpeg",
   },
   {
     id: 21,
     name: 'Rolex Submariner Watch',
     price: 10999,
-    image: "New folder/download (3).jpeg",
+    image: "../assets/products/download (3).jpeg",
   },
   {
     id: 22,
     name: 'Canon EOS R5 Camera',
     price: 3899,
-    image: "New folder/download (5).jpeg",
+    image: "../assets/products/download (5).jpeg",
   },
   {
     id: 23,
     name: 'Bose SoundLink Bluetooth Speaker',
     price: 299,
-    image: "New folder/69-694768_amazon-echo-png-clipart-transparent-amazon-echo-png 1.png",
+    image: "../assets/products/69-694768_amazon-echo-png-clipart-transparent-amazon-echo-png 1.png",
   },
 
 ];

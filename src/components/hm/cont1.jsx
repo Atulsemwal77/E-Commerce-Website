aa50b2c10001155f37eef12f234446315840588b
Atul semwal <atulsemwal332@gmail.com>
2025-01-25 13:59:41 +0530

    change

--- src/components/hm/cont1.jsx
+++ src/components/hm/cont1.jsx
@@ -1,4 +1,6 @@
-
+import slide1 from '../../assets/products/hero_endframe__cvklg0xk3w6e_large 2.png';
+import slide2 from '../../assets/products/hero_endframe__cvklg0xk3w6e_large 2.png';
+import slide3 from '../../assets/products/hero_endframe__cvklg0xk3w6e_large 2.png';
 const Cont1 = ()=>{
     return(
         <>
@@ -31,21 +33,21 @@ const Cont1 = ()=>{
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="New folder\hero_endframe__cvklg0xk3w6e_large 2.png" class="d-block w-100" alt="..."/>
+      <img src= {slide1} class="d-block w-100" alt="..."/>
       <div class="carousel-caption">
         <h5>First slide label</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="New folder\hero_endframe__cvklg0xk3w6e_large 2.png" class="d-block w-100" alt="..."/>
+      <img src={slide2} class="d-block w-100" alt="..."/>
       <div class="carousel-caption d-none d-md-block">
         <h5>Second slide label</h5>
         <p>Some representative placeholder content for the second slide.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="New folder\hero_endframe__cvklg0xk3w6e_large 2.png" class="d-block w-100" alt="..."/>
+      <img src={slide3} class="d-block w-100" alt="..."/>
       <div class="carousel-caption d-none d-md-block">
         <h5>Third slide label</h5>
         <p>Some representative placeholder content for the third slide.</p>
